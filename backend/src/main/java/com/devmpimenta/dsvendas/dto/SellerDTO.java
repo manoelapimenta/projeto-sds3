4dead0c994df9245fce6a212489511feeb199aee
manoelapimenta <manoela.pimenta@gmail.com>
2021-05-05 22:22:18 -0300

    Camadas

---
+++ backend/src/main/java/com/devmpimenta/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,39 @@
+package com.devmpimenta.dsvendas.dto;
+
+import com.devmpimenta.dsvendas.entities.Seller;
+
+public class SellerDTO {
+	
+	private Long id;
+	private String name;
+	
+	public SellerDTO( ) {
+}
+
+	public SellerDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public SellerDTO(Seller entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
